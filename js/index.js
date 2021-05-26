583833dfaad27e829eefed2d6850d18d22bc9c91
pistard <yannaubertin@hotmail.fr>
2021-05-26 09:24:51 +0200

    création js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+function onDOMLoaded(){
+    var baliseLoad=document.getElementById('js-loaded');
+    baliseLoad.style.backgroundColor="skyblue";
+    baliseLoad.innerHTML="JS chargé !!!";
+}
+
+onDOMLoaded();
