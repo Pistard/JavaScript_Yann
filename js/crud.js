6367ff0a030a0b938f63b66842b51c1ea93ccac9
pistard <yannaubertin@hotmail.fr>
2021-05-27 12:26:58 +0200

    js+

--- js/crud.js
+++ js/crud.js
@@ -5,17 +5,25 @@ function xhr(url, callback, method) {
     }
     var xhr = new XMLHttpRequest()
     xhr.open(method, url)
-    xhr.open('GET', 'vues/tchat.html')
+    // xhr.open('GET', 'vues/tchat.html')
     xhr.onreadystatechange = function () {
         if (xhr.readyState < XMLHttpRequest.DONE) return;
         if (xhr.status > 400) return;
-        document.querySelector('#main').innerHTML = xhr.response;
+        // document.querySelector('#main').innerHTML = xhr.response;
         callback(xhr.response)
         console.log(xhr.response);
     }
     xhr.send()
 }
 
-xhr('vues/tchat.html', function (resp) {
-    document.querySelector('#main').innerHTML = resp;
-});
+xhr('vues/tchat.html',
+    function (resp) { 
+        document.querySelector('#main').innerHTML = resp; }
+);
+
+xhr('http://localhost:5629/messages?_expand=user',
+    function (fluxJsonDuServer) {
+        var arr = JSON.parse(fluxJsonDuServer);
+        console.log(arr);
+    }
+)
